2b04b455fdca69538750f80382838eab24fba0ae
apavanello <apavanello@live.com>
2018-05-16 19:54:44 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo yum install zsh -y
-yes | cp .zshcr ~/.zshcr
+yes | cp ./.zshcr ~/.zshcr
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 echo "Be Happy"
 zsh
